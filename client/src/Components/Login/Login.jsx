98088bf44c3ccc0cd7ded8819ca13767cc3d3271
Erue <ekpenisiraphael@gmail.com>
2023-06-29 21:01:37 +0530

    Login frontend implemented.

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-    login  
-    </div>
+    <main>
+    <form className="form">
+    <h1 className="formTitle">Log in into your account</h1>
+    <span className="input-span">
+    <label for="email" className="label">Email</label>
+    <input type="email" name="email" id="email" /></span>
+    <span className="input-span" >
+    <label for="password" className="label">Password</label>
+    <input type="password" name="password" id="password" /></span>
+    <span className="span"><a href="#">Forgot password?</a></span>
+    <input className="submit" type="submit" value="Log in" />
+    <span className="span">Don't have an account? <Link to="/signup">Sign up</Link></span>
+  </form>
+  </main>
   )
 }
 
