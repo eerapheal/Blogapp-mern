4a92af5a6125aac2cc8619646198cba35ad58856
Erue <ekpenisiraphael@gmail.com>
2023-06-29 21:02:18 +0530

    Sign up frontend implemented.

--- client/src/Components/SignUp/SignUp.jsx
+++ client/src/Components/SignUp/SignUp.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>
-      signup
-    </div>
+    <main>
+    <form className="form">
+        <h1 className="formTitle">Sign up new user</h1>
+    <span className="input-span">
+    <label for="email" className="label">Email</label>
+    <input type="email" name="email" id="email" /></span>
+    <span className="input-span">
+    <label for="password" className="label">Password</label>
+    <input type="password" name="password" id="password" /></span>
+    <input className="submit" type="submit" value="Sign Up" />
+    <span className="span">You already have an account? <Link to="/login">Sign in</Link></span>
+  </form>
+    </main>
+
   )
 }
 
