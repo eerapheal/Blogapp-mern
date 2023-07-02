b221c97700198ab06e53cd6104d6b3b5728f4c95
Erue <ekpenisiraphael@gmail.com>
2023-07-02 17:22:24 +0530

    Password and login email set to onChange.

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  // const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <main>
-    <form className="form">
-    <h1 className="formTitle">Log in into your account</h1>
-    <span className="input-span">
-    <label for="email" className="label">Email</label>
-    <input type="email" name="email" id="email" /></span>
-    <span className="input-span" >
-    <label for="password" className="label">Password</label>
-    <input type="password" name="password" id="password" /></span>
-    <span className="span"><a href="#">Forgot password?</a></span>
-    <input className="submit" type="submit" value="Log in" />
-    <span className="span">Don't have an account? <Link to="/signup">Sign up</Link></span>
-  </form>
-  </main>
-  )
-}
+      <form className="form">
+        <h1 className="formTitle">Log in into your account</h1>
+        <span className="input-span">
+          <label for="email" className="label">
+            Email
+          </label>
+          <input type="email" name="email" id="email" value={email}
+            onChange={(ev) => setEmail(ev.target.value)} />
+        </span>
+        <span className="input-span">
+          <label for="password" className="label">
+            Password
+          </label>
+            onChange={(ev) => setPassword(ev.target.value)}
+          <input type="password" name="password" id="password" value={password}
+           onChange={(ev) => setPassword(ev.target.value)} />
+        </span>
+        <span className="span">
+          <a href="#">Forgot password?</a>
+        </span>
+        <input className="submit" type="submit" value="Log in" />
+        <span className="span">
+          Don't have an account? <Link to="/signup">Sign up</Link>
+        </span>
+      </form>
+    </main>
+  );
+};
 
-export default Login
+export default Login;
