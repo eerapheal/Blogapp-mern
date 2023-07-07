13903e24be9cd0399f3837804639a6ba24412e34
Erue <ekpenisiraphael@gmail.com>
2023-07-08 02:53:15 +0530

    Active user redirect to post.

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useEffect } from 'react'
 
 const Header = () => {
-useEffect(() => {
-fetch('http://localhost:4000/profile', {
-  credentials: "include",
-}).then(response => {
-    response.json().then(usersInfo => {
-
+  const [email, setEmail] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setEmail(userInfo.email);
+      });
     });
-})
-},[])
+  }, []);
 
   const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
   const [menu_class, setMenuClass] = useState("menu hidden");
@@ -29,175 +29,200 @@ fetch('http://localhost:4000/profile', {
   };
 
   return (
-
-      <nav>
-        <div className="mobileMenuDiv" style={{ width: "100%" }}>
-          <nav className="mobileMenu">
-            <div className="burger-menu" onClick={updateMenu}>
-              <div className={burger_class}></div>
-              <div className={burger_class}></div>
-              <div className={burger_class}></div>
-            </div>
-            <div>
-            <Link to="/" className="logo" alt="Logo">
-              Sant<span className="logoB">B</span>log
-            </Link>
+    <nav>
+      <div className="mobileMenuDiv" style={{ width: "100%" }}>
+        <nav className="mobileMenu">
+          <div className="burger-menu" onClick={updateMenu}>
+            <div className={burger_class}></div>
+            <div className={burger_class}></div>
+            <div className={burger_class}></div>
           </div>
-          </nav>
-          <div className={menu_class}>
-          <ul className="mobilenavItem">
-            <li className="mobilenavlinks">
-                <Link to="/technology" alt="Technology" className="navlinks">
-                  Technology
-                </Link>
-              </li>
-              <li className="mobilenavlinks">
-                <Link to="/lifestyle" alt="Lifestyle" className="navlinks">
-                 Lifestyle
-                </Link>
-              </li>
-              <li className="mobilenavlinks">
-                <Link to="/health" alt="Health" className="navlinks">
-                  Health
-                </Link>
-              </li>
-              <li className="mobilenavlinks">
-                <Link to="/treadingnews" alt="treadingNews">
-                 Treading News
-                 <i class="fas fa-caret-down"></i>
-                </Link>
-                <div className="dropdownMenu">
-                  <ul>
-                    <li>
-                      <a href="" alt="Global news">General News</a>
-                      </li>
-                    <li>
-                      <a href="" alt="Sport">
-                        Sport
-                        <i class="fas fa-caret-right"></i>
-                        <div className="dropdownMenu1">
-                  <ul>
-                    <li>
-                      <a href="" alt="Global news">Football</a>
-                      </li>
-                    <li>
-                      <a href="" alt="UFC/MMA">
-                        UFC/MMA
-                        </a>
-                      </li>
-                    <li>
-                      <a href="" alt="Rugby">Rugby</a>
-                      </li>
-                    <li>
-                      <a href="" alt="Cricket">Cricket</a>
-                      </li>
-                  </ul>
-                </div>
-                        </a>
-                      </li>
-                    <li>
-                      <a href="" alt="Entern">Enterntement</a>
-                      </li>
-                    <li>
-                      <a href="" alt="Discovery">Discovery</a>
-                      </li>
-                  </ul>
-                </div>
-              </li>
-              <li className="mobilenavlinks">
-                <Link to="/login" alt="Login">
-                  Login
-                </Link>
-              </li>
-              <li className="mobilenavlinks">
-                <Link to="/signup" alt="Signup" className="navlinks">
-                  Sign Up
-                </Link>
-              </li>
-            </ul>
-            </div>
-        </div>
-        <div className="headerDesk">
           <div>
             <Link to="/" className="logo" alt="Logo">
               Sant<span className="logoB">B</span>log
             </Link>
           </div>
-          <nav>
-            <ul className="navItem">
-            <li className="navlinks">
-                <Link to="/Technology" alt=" Technology" className="navlinks">
-                  Technology
-                </Link>
-              </li>
-              <li className="navlinks">
-                <Link to="/lifestyle" alt="Lifestyle" className="navlinks">
-                 Lifestyle
-                </Link>
-              </li>
-              <li className="navlinks">
-                <Link to="/health" alt="Health" className="navlinks">
-                  Health
-                </Link>
-              </li>
-              <li className="navlinks">
-                <Link to="/treandingNews" alt="treandingNews" className="navlinks1">
-                 Treading News
-                 <i class="fas fa-caret-down"></i>
-                </Link>
-                <div className="dropdownMenu">
-                  <ul>
-                    <li>
-                      <a href="" alt="Global news">Global news</a>
-                      </li>
-                    <li>
-                      <a href="" alt="Sport">
-                        Sport
-                        <i class="fas fa-caret-right"></i>
-                        <div className="dropdownMenu1">
-                  <ul>
-                    <li>
-                      <a href="" alt="Global news">Football</a>
-                      </li>
-                    <li>
-                      <a href="" alt="UFC/MMA">
-                        UFC/MMA
-                        </a>
-                      </li>
-                    <li>
-                      <a href="" alt="Rugby">Rugby</a>
-                      </li>
-                    <li>
-                      <a href="" alt="Cricket">Cricket</a>
-                      </li>
-                  </ul>
-                </div>
-                        </a>
-                      </li>
-                    <li>
-                      <a href="" alt="Entern">Enterntement</a>
-                      </li>
-                    <li>
-                      <a href="" alt="Discovery">Discovery</a>
-                      </li>
-                  </ul>
-                </div>
-              </li>
-              <li className="navlinks">
-                <Link to="/login" alt="Login">
-                  Login
-                </Link>
-              </li>
-              <li className="navlinks">
-                <Link to="/signup" alt="Signup" className="navlinks">
-                  Sign Up
-                </Link>
-              </li>
-            </ul>
-          </nav>
+        </nav>
+        <div className={menu_class}>
+          <ul className="mobilenavItem">
+            <li className="mobilenavlinks">
+              <Link to="/technology" alt="Technology" className="navlinks">
+                Technology
+              </Link>
+            </li>
+            <li className="mobilenavlinks">
+              <Link to="/lifestyle" alt="Lifestyle" className="navlinks">
+                Lifestyle
+              </Link>
+            </li>
+            <li className="mobilenavlinks">
+              <Link to="/health" alt="Health" className="navlinks">
+                Health
+              </Link>
+            </li>
+            <li className="mobilenavlinks">
+              <div alt="treadingNews">
+                Treading News <i className="fas fa-caret-down"></i>
+              </div>
+              <div className="dropdownMenu">
+                <ul>
+                  <li>
+                    <Link to="/" alt="Global news">
+                      General News
+                    </Link>
+                  </li>
+                  <li>
+                    <div alt="Sport">
+                      Sport <i className="fas fa-caret-right"></i>
+                    </div>
+                    <div className="dropdownMenu1">
+                      <ul>
+                        <li>
+                          <Link to="/" alt="Football">
+                            Football
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/" alt="UFC/MMA">
+                            UFC/MMA
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/" alt="Rugby">
+                            Rugby
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/" alt="Cricket">
+                            Cricket
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <Link to="/" alt="Entern">
+                      Enterntement
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/" alt="Discovery">
+                      Discovery
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="mobilenavlinks">
+              <Link to="/login" alt="Login">
+                Login
+              </Link>
+            </li>
+            <li className="mobilenavlinks">
+              <Link to="/signup" alt="Signup" className="navlinks">
+                Sign Up
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-
+      </div>
+      <div className="headerDesk">
+        <div>
+          <Link to="/" className="logo" alt="Logo">
+            Sant<span className="logoB">B</span>log
+          </Link>
+        </div>
+        <nav>
+          <ul className="navItem">
+            <li className="navlinks">
+              <Link to="/Technology" alt=" Technology" className="navlinks">
+                Technology
+              </Link>
+            </li>
+            <li className="navlinks">
+              <Link to="/lifestyle" alt="Lifestyle" className="navlinks">
+                Lifestyle
+              </Link>
+            </li>
+            <li className="navlinks">
+              <Link to="/health" alt="Health" className="navlinks">
+                Health
+              </Link>
+            </li>
+            <li className="navlinks">
+              <div alt="treandingNews" className="navlinks">
+                Treading News <i className="fas fa-caret-down"></i>
+              </div>
+              <div className="dropdownMenu">
+                <ul>
+                  <li>
+                    <Link to="/" alt="Global news">
+                      Global news
+                    </Link>
+                  </li>
+                  <li>
+                    <div alt="Sport">
+                      Sport <i className="fas fa-caret-right"></i>
+                    </div>
+                    <div className="dropdownMenu1">
+                      <ul>
+                        <li>
+                          <Link to="/" alt="Football">
+                            Football
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/" alt="UFC/MMA">
+                            UFC/MMA
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/" alt="Rugby">
+                            Rugby
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/" alt="Cricket">
+                            Cricket
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <Link to="/" alt="Entern">
+                      Enterntement
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/" alt="Discovery">
+                      Discovery
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="navlinks">
+              {email && (
+                <>
+                  <Link to="/create">make post</Link>
+                </>
+              )}
+              {!email && (
+                <>
+                  <Link to="/login" alt="Login">
+                    Login
+                  </Link>
+                  <Link to="/signup" alt="Signup" className="navlinks">
+                    Sign Up
+                  </Link>
+                </>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </nav>
   );
 };
 
