5f54fb27bb9888e7be4cc5d99931c9016066ebe2
Erue <ekpenisiraphael@gmail.com>
2023-07-08 15:40:05 +0530

    Create post form added.

--- client/src/Components/Create/Create.jsx
+++ client/src/Components/Create/Create.jsx
@@ -1,6 +1,13 @@
 const Create = () => {
     return (
-        <h1>Create</h1>
+        <main>
+        <form>
+            <input type="title" placeholder="Title"/>
+            <input type="summary" placeholder="Summary"/>
+            <input type="file" />
+            <textarea name="text" id="text" cols="30" row="10" ></textarea>
+        </form>
+       </main>
     )
 }
 
