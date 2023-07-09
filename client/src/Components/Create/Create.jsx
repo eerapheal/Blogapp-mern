44c224bf019b09ec02188ac2d0be588dfcb70485
Erue <ekpenisiraphael@gmail.com>
2023-07-09 10:14:51 +0530

    ReactQuill implementation.

--- client/src/Components/Create/Create.jsx
+++ client/src/Components/Create/Create.jsx
@@ -1,11 +1,44 @@
+import React, {useState} from 'react'
+import ReactQuill from 'react-quill'
+import "react-quill/dist/quill.snow.css"
+
+const modules ={
+    toolbar: [
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ align: [] }],
+    
+        [{ list: 'ordered'}, { list: 'bullet' }],
+        [{ indent: '-1'}, { indent: '+1' }],
+    
+        ['link', 'image', 'video'],
+        [{ color: [] }, { background: [] }],
+   
+      ],
+}; 
+
+const formats = [
+    'bold', 'italic', 'underline', 'strike',
+    'align', 'list', 'indent',
+    'size', 'header',
+    'link', 'image', 'video',
+    'color', 'background',
+    'clean',
+  ]
+  
 const Create = () => {
+    const [title, setTitle] =useState("");
+    const [summary, setsummary] =useState("");
+    const [content, setContent] =useState("");
+
     return (
         <main>
-        <form>
-            <input type="title" placeholder="Title"/>
-            <input type="summary" placeholder="Summary"/>
+        <form >
+            <input type="title" placeholder={"Title"} />
+            <input type="summary" placeholder={'Summary'} />
             <input type="file" />
-            <textarea name="text" id="text" cols="30" row="10" ></textarea>
+            <ReactQuill value={content} modules={modules} formats={formats} />
+            <button type="submit">Create post</button>
         </form>
        </main>
     )
