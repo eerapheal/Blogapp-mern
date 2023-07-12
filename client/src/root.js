11854215dac8627e45fa0f8c5a0aa4d30f08cfd8
Erue <ekpenisiraphael@gmail.com>
2023-07-12 08:29:17 +0530

    Post enroute

--- client/src/root.js
+++ client/src/root.js
@@ -1,40 +1,38 @@
 import React from "react";
 import Header from "./Components/Header/Header";
-import Post from "./Components/Post/Post";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./Components/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login/Login";
 import SignUp from "./Components/SignUp/SignUp";
-import TreadingNews from './Components/TreadingNews/TreadingNews'
-import Technology from './Components/Technology/Technology'
-import Health from './Components/Health/Health'
-import Lifestyle from './Components/Lifestyle/Lifestyle'
-import Gramarlly from './Components/Gramarlly/Gramarlly'
-import Create from './Components/Create/Create'
+import TreadingNews from "./Components/TreadingNews/TreadingNews";
+import Technology from "./Components/Technology/Technology";
+import Health from "./Components/Health/Health";
+import Lifestyle from "./Components/Lifestyle/Lifestyle";
+import Gramarlly from "./Components/Gramarlly/Gramarlly";
+import CreatePost from "./Components/CreatePost/CreatePost";
 // import userContextProvider from './userContext'
-
 
 const Root = () => {
   return (
-// {/* <userContextProvider> */}
+    // {/* <userContextProvider> */}
     <Router>
       <header>
         <Header />
       </header>
 
-<Routes>
-          <Route path="/" element={<Post />} />
-          <Route path="/technology" element={<Technology />} />
-          <Route path="/lifestyle" element={<Lifestyle />} />
-          <Route path="/health" element={<Health />} />
-          <Route path="/treadingnews" element={<TreadingNews />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/gramarlly" element={<Gramarlly />} />
-          <Route path="/create" element={<Create />} />
-        </Routes>
-           </Router>
-// </userContextProvider>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/technology" element={<Technology />} />
+        <Route path="/lifestyle" element={<Lifestyle />} />
+        <Route path="/health" element={<Health />} />
+        <Route path="/treadingnews" element={<TreadingNews />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/gramarlly" element={<Gramarlly />} />
+        <Route path="/createPost" element={<CreatePost />} />
+      </Routes>
+    </Router>
+    // </userContextProvider>
   );
 };
 
