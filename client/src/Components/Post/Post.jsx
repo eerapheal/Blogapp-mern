9a820d498217c468070819b542cb6b6fced015f1
Erue <ekpenisiraphael@gmail.com>
2023-07-12 08:28:17 +0530

    Post componenet fatorize.

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -1,70 +1,26 @@
 import React from "react";
-import ralph from "../../Assets/images/ralph.jpg";
-import docas from "../../Assets/images/docas.jpg";
-import chiyerem from "../../Assets/images/chiyerem.jpg";
+import { formatISO9075 } from 'date-fns';
 
-const Post = () => {
+const Post = ({ title, summary, cover, content, createdAt }) => {
+  const date = createdAt instanceof Date ? createdAt : new Date(createdAt);
+
   return (
     <main>
-    <div className="post">
-      <div className="mainPost">
-      <div className="imgpost">
-        <img src={ralph} alt="ralph" />
-        </div>
-        <div className="pst">
-          <h2 className="postTitle">
-            “Pitch Yourself”
-          </h2>
-          <p  className="info">
-            <a className="author">Ralph </a>
-            <time>29-06-2023 10:45</time>
-          </p>
-          <p className="postParagh">
-            “Hello Dave, I'd like to help you with your app idea. I'll build
-            your app as per your requirements and help you achieve your
-            development goals.
-          </p>
+      <div className="post">
+        <div className="mainPost">
+          <div className="imgpost">
+            <img src={cover} alt="cover" />
+          </div>
+          <div className="pst">
+            <h2 className="postTitle">{title}</h2>
+            <p className="info">
+              <a className="author">Ralph</a>
+              <time>{formatISO9075(date)}</time>
+            </p>
+            <p className="postParagh">{summary}</p>
+          </div>
         </div>
       </div>
-      <div className="mainPost">
-      <div className="imgpost">
-        <img src={docas} alt="docas" />
-        </div>
-        <div className="pst">
-          <h2 className="postTitle">
-               “Pitch Yourself”
-          </h2>
-          <p  className="info">
-            <a className="author">Dorcas </a>
-            <time>29-06-2023 10:45</time>
-          </p>
-          <p className="postParagh">
-            “Hello Dave, I'd like to help you with your app idea. I'll build
-            your app as per your requirements and help you achieve your
-            development goals.
-          </p>
-        </div>
-      </div>
-      <div className="mainPost">
-      <div className="imgpost">
-        <img src={chiyerem} alt="chiyerem" />
-        </div>
-        <div className="pst">
-          <h2 className="postTitle">
-          “Pitch Yourself”
-          </h2>
-          <p  className="info">
-            <a className="author"> Chiyerem Erue </a>
-            <time>29-06-2023 10:45</time>
-          </p>
-          <p className="postParagh">
-            “Hello Dave, I'd like to help you with your app idea. I'll build
-            your app as per your requirements and help you achieve your
-            development goals.
-          </p>
-        </div>
-      </div>
-    </div>
     </main>
   );
 };
