83778a332ce0560630a16643ef841e757285e4e8
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-08-25 15:52:27 +0530

    Redux store provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Root from "./root";
+import { Provider } from 'react-redux';
+import store from './Components/store/store';
+
 import {UserContextProvider} from './UserContext'
 const App = () => {
   return (
+    <Provider store={store}>
     <UserContextProvider>
     
     <Root />
-  </UserContextProvider>);
-
+  </UserContextProvider>
+  </Provider>
+  );
 };
 
 export default App;
