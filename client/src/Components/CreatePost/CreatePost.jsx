7b6ca963ff6e94cd5d5e7063b595a10a5e66c534
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-08-27 20:20:22 +0530

    Category add to create post and posts created.

--- client/src/Components/CreatePost/CreatePost.jsx
+++ client/src/Components/CreatePost/CreatePost.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 
 const CreatePost = () => {
@@ -6,7 +6,27 @@ const CreatePost = () => {
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
   const [files, setFiles] = useState("");
+  const [category, setCategory] = useState("");
+  const [categories, setCategories] = useState([]);
   const [redirect, setRedirect] = useState("");
+
+  useEffect(() => {
+    // Fetch the list of categories from your server when the component mounts
+    fetch("http://localhost:4000/categories", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+        if (data.length > 0) {
+          setCategory(data[0]._id); // Set the default category to the first category in the list
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+      });
+  }, []);
 
   const makePost = async (ev) => {
     ev.preventDefault();
@@ -15,6 +35,7 @@ const CreatePost = () => {
     data.set("summary", summary);
     data.set("content", content);
     data.set("file", files[0]);
+    data.set("categoryId", category);
     const response = await fetch("http://localhost:4000/post", {
       method: "POST",
       body: data,
@@ -44,6 +65,16 @@ const CreatePost = () => {
           value={summary}
           onChange={(ev) => setSummary(ev.target.value)}
         />
+        <select
+          value={category}
+          onChange={(ev) => setCategory(ev.target.value)}
+        >
+          {categories.map((cat) => (
+            <option key={cat._id} value={cat._id}>
+              {cat.name}
+            </option>
+          ))}
+        </select>
         <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
         <textarea
           placeholder={" Write Your Content Here"}
