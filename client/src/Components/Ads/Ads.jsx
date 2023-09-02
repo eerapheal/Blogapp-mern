e07d53401568b610ad394b57ebf059d1e55c8f08
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-09-02 15:13:39 +0530

    One ads removed

--- client/src/Components/Ads/Ads.jsx
+++ client/src/Components/Ads/Ads.jsx
@@ -1,13 +1,10 @@
-import ads from '../../Assets/images/ad-01.jpg';
+// import ads from '../../Assets/images/ad-01.jpg';
 import adss from '../../Assets/images/3086583936293164863.gif';
 import './Ads.css'
 const Ads = () => {
   return (
     <div>
       <div className="adsTop">
-        <div >
-					<a href='example.com'><img src={ads} alt='ads' className="adsLeft" /></a>
-				</div>
         <div>
 				<a href='example.com'><img src={adss} alt='adss' className="adsRight"/></a>
 				</div>
