3cf5910970b53d361b3018ab9bd1965155d1a4e2
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-09-03 01:23:37 +0530

    View more function add to post

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 import { formatISO9075 } from "date-fns";
 import "./Post.css";
 
-const Post = ({ _id, title, summary, cover, createdAt, author }) => {
+const Post = () => {
   const [posts, setPosts] = useState([]);
+  const [visiblePosts, setVisiblePosts] = useState(10); // Number of posts to initially display
 
   useEffect(() => {
     // Move the fetch call to a separate useEffect to avoid infinite rendering
@@ -27,41 +28,54 @@ const Post = ({ _id, title, summary, cover, createdAt, author }) => {
     fetchData();
   }, []);
 
-  let date = new Date(); // Initialize with a default value
+  const loadMorePosts = () => {
+    // Increase the number of visible posts based on the device type
+    setVisiblePosts((prevVisiblePosts) =>
+      window.innerWidth < 768 ? prevVisiblePosts + 10 : prevVisiblePosts + 40
+    );
+  };
 
-  if (typeof createdAt === "string" && !isNaN(Date.parse(createdAt))) {
-    date = new Date(createdAt);
-  } else if (createdAt instanceof Date) {
-    date = createdAt;
-  }
+  let date = new Date(); // Initialize with a default value
 
   return (
     <div>
       {posts.length > 0 &&
-        posts.map((post) => (
-          <div key={post._id} className="pst">
-            <div className="postCard stack">
-              <Link to={`/post/${post._id}`}>
-                <img
-                  className="imgpost"
-                  src={`http://localhost:4000/${post.cover}`}
-                  alt="cover"
-                />
-              </Link>
-              <div className="postContent">
-                <h2 className="postTitle">{post.title}</h2>
-                <p className="postParagh">{post.summary}</p>
-                <p className="info">
-                  {post.author && (
-                    <span className="author">{post.author.username}</span>
-                  )}
-                  {" "}
-                  <time>{formatISO9075(date)}</time>
-                </p>
+        posts.slice(0, visiblePosts).map((post) => {
+          // Date handling as in the original code
+          if (typeof post.createdAt === "string" && !isNaN(Date.parse(post.createdAt))) {
+            date = new Date(post.createdAt);
+          } else if (post.createdAt instanceof Date) {
+            date = post.createdAt;
+          }
+
+          return (
+            <div key={post._id} className="pst">
+              <div className="postCard stack">
+                <Link to={`/post/${post._id}`}>
+                  <img
+                    className="imgpost"
+                    src={`http://localhost:4000/${post.cover}`}
+                    alt="cover"
+                  />
+                </Link>
+                <div className="postContent">
+                  <h2 className="postTitle">{post.title}</h2>
+                  <p className="postParagh">{post.summary}</p>
+                  <p className="info">
+                    {post.author && (
+                      <span className="author">{post.author.username}</span>
+                    )}
+                    {" "}
+                    <time>{formatISO9075(date)}</time>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
+      {visiblePosts < posts.length && (
+        <button onClick={loadMorePosts}>View More</button>
+      )}
     </div>
   );
 };
