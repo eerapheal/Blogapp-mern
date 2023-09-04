16e80889899f9e92e92f7e67e87994c4db449c9c
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-09-04 14:28:24 +0530

    final adjustment

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -31,7 +31,7 @@ const Post = () => {
   const loadMorePosts = () => {
     // Increase the number of visible posts based on the device type
     setVisiblePosts((prevVisiblePosts) =>
-      window.innerWidth < 768 ? prevVisiblePosts + 10 : prevVisiblePosts + 40
+      window.innerWidth < 2000 ? prevVisiblePosts + 12 : prevVisiblePosts + 40
     );
   };
 
@@ -39,40 +39,46 @@ const Post = () => {
 
   return (
     <div>
-      {posts.length > 0 &&
-        posts.slice(0, visiblePosts).map((post) => {
-          // Date handling as in the original code
-          if (typeof post.createdAt === "string" && !isNaN(Date.parse(post.createdAt))) {
-            date = new Date(post.createdAt);
-          } else if (post.createdAt instanceof Date) {
-            date = post.createdAt;
-          }
+      <div className="postContanier">
+        {posts.length > 0 &&
+          posts.slice(0, visiblePosts).map((post) => {
+            // Date handling as in the original code
+            if (
+              typeof post.createdAt === "string" &&
+              !isNaN(Date.parse(post.createdAt))
+            ) {
+              date = new Date(post.createdAt);
+            } else if (post.createdAt instanceof Date) {
+              date = post.createdAt;
+            }
 
-          return (
-            <div key={post._id} className="pst">
-              <div className="postCard stack">
-                <Link to={`/post/${post._id}`}>
-                  <img
-                    className="imgpost"
-                    src={`http://localhost:4000/${post.cover}`}
-                    alt="cover"
-                  />
-                </Link>
-                <div className="postContent">
-                  <h2 className="postTitle">{post.title}</h2>
-                  <p className="postParagh">{post.summary}</p>
-                  <p className="info">
-                    {post.author && (
-                      <span className="author">{post.author.username}</span>
-                    )}
-                    {" "}
-                    <time>{formatISO9075(date)}</time>
-                  </p>
+            return (
+              <div className="HeadlineWraper">
+                <div key={post._id}>
+                  <div className=" postCard stack">
+                    <Link to={`/post/${post._id}`}>
+                      <img
+                        className="imgpost"
+                        src={`http://localhost:4000/${post.cover}`}
+                        alt="cover"
+                      />
+                    </Link>
+                    <div className="postContent">
+                      <h2 className="postTitle">{post.title}</h2>
+                      <p className="postParagh">{post.summary}</p>
+                      <p className="info">
+                        {post.author && (
+                          <span className="author">{post.author.username}</span>
+                        )}{" "}
+                        <time>{formatISO9075(date)}</time>
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+      </div>
       {visiblePosts < posts.length && (
         <button onClick={loadMorePosts}>View More</button>
       )}
