2809348e54ae0ffbf7adc92176692dc010e5280d
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-09-04 12:51:09 +0530

    Date time format add

--- client/src/Components/NewsList/NewsList.js
+++ client/src/Components/NewsList/NewsList.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { fetchNews } from '../../Redux/NewsActions';
-import './NewsList.css'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchNews } from "../../Redux/NewsActions";
+import { formatISO9075 } from "date-fns";
+import "./NewsList.css";
 
-const NewsList = ({ news, loading, error, fetchNews }) => {
+const NewsList = ({ news, loading, error, publishedAt, fetchNews }) => {
   useEffect(() => {
     fetchNews();
   }, []);
@@ -15,24 +16,35 @@ const NewsList = ({ news, loading, error, fetchNews }) => {
   if (error) {
     return <p>Error: {error}</p>;
   }
+  let date = new Date(); // Initialize with a default value
+
+  if (typeof publishedAt === "string" && !isNaN(Date.parse(publishedAt))) {
+    date = new Date(publishedAt);
+  } else if (publishedAt instanceof Date) {
+    date = publishedAt;
+  }
 
   return (
-    <div style={{paddingTop: '75px'}} className='container'>
-      <h2 className='Headlines'>Top Headlines</h2>
-      <ul  className='HeadlineWraper'>
+    <div style={{ paddingTop: "75px" }} className="container">
+      <h2 className="Headlines">Top Headlines</h2>
+      <ul className="HeadlineWraper">
         {news.map((article) => (
-          <li key={article.title} className='newsCard stack featured'>
-            
-            <img className='newsImg'src={article.urlToImage} alt="Article Thumbnail" />
-            <div className='cardContent'>
+          <li key={article.title} className="newsCard stack featured">
+            <img
+              className="newsImg"
+              src={article.urlToImage}
+              alt="Article Thumbnail"
+            />
+            <div className="cardContent">
               <p>{article.title}</p>
-              <p>Date {article.publishedAt}</p>
-            <p>
-              <a href={article.url}>
-                Read more
-              </a>
-            </p>
-           </div>
+              {/* <p>Date {article.publishedAt}</p> */}
+              <time className="dateTime">{formatISO9075(date)}</time>
+              <p>
+                <a className="readMore" href={article.url}>
+                  Read more
+                </a>
+              </p>
+            </div>
           </li>
         ))}
       </ul>
