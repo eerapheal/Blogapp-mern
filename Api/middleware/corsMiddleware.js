7488b3567424ae4adedca0ef4d91a5cdc99c4bc3
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-10-18 04:00:19 +0530

    origin reset

--- Api/middleware/corsMiddleware.js
+++ Api/middleware/corsMiddleware.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 
 const corsMiddleware = cors({
   credentials: true,
-  origin: "*",
+  origin: ["https://santmagazine.netlify.app"],
 });
 
 module.exports = corsMiddleware;
