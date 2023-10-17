6abea3c5e80d5f8e041a2f9a8542dd4c53a1ce0a
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-10-18 05:09:45 +0530

    localhost added

--- Api/middleware/corsMiddleware.js
+++ Api/middleware/corsMiddleware.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 
 const corsMiddleware = cors({
   credentials: true,
-  origin: ["https://santmagazine.netlify.app"],
+  origin: ["http://localhost:3000", "https://santmagazine.netlify.app"],
 });
 
 module.exports = corsMiddleware;
